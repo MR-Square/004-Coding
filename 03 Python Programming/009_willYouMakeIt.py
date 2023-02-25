510e29133a63fd7cd43854b18bca257a4640b942
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2023-02-25 11:33:41 +0530

    wiil you make it

---
+++ 03 Python Programming/009_willYouMakeIt.py
@@ -0,0 +1,14 @@
+"""
+You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is 
+running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon.
+There are 2 gallons left.Considering these factors, write a function that tells you if it is possible to get to the pump 
+or not.Function should return true if it is possible and false if not.
+"""
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return True if (mpg * fuel_left) >= distance_to_pump else False
+
+
+if __name__ == '__main__':
+    my_factors = list(input("enter the factors:").split())
+    print(zero_fuel(int(my_factors[0]),int(my_factors[1]),int(my_factors[2])))
