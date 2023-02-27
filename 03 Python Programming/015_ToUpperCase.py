fcb8a2922ebfd94bbbaa86c915bfd518010a7c6e
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2023-02-27 11:46:47 +0530

    convert to upper case

---
+++ 03 Python Programming/015_ToUpperCase.py
@@ -0,0 +1,9 @@
+# Write a function which converts the input string to uppercase.
+
+
+def to_uppercase(s):
+    return s.upper()
+
+if __name__ == '__main__':
+    s = input('enter your string::')
+    print(to_uppercase(s))
